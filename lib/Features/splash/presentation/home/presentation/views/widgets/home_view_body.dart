3afaa6cf9537706b8e5bfd283be374d26e830129
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-08-05 14:56:54 +0300

    create styles file

--- lib/Features/splash/presentation/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/splash/presentation/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class HomeViewbody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
             selectionColor: Colors.white,
           ),
           BestSellerListViewItem(),
--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,6 @@ import 'package:flutter/services.dart';
 const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(seconds: 1);
 const kTransitionDelay = Duration(milliseconds: 250);
+const kGtSectraFine = 'Gt Sectra Fine';
+const kGilroyBold = 'Gilroy-Bold';
+ 
