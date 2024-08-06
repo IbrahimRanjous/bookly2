21900b0cb5a54715ac237de9d9aaf46648d9eb65
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-08-06 20:36:01 +0300

    create similar books list view

--- lib/Features/splash/presentation/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/splash/presentation/home/presentation/views/widgets/book_details_view_body.dart
@@ -2,8 +2,8 @@ import 'package:bookly/Features/splash/presentation/home/presentation/views/widg
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/core/widgets/custom_botton.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -49,6 +49,23 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 37,
           ),
           const BooksAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
+          const SizedBox(
+            height: 40,
+          ),
         ],
       ),
     );
