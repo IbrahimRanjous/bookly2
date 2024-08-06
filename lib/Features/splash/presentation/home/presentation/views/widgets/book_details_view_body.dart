093d1d015cb78135c7f6920be626376fe03be624
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-08-06 20:19:19 +0300

    create books button action

--- lib/Features/splash/presentation/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/splash/presentation/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly/Features/splash/presentation/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/core/widgets/custom_botton.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -43,7 +45,10 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
-          
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
         ],
       ),
     );
