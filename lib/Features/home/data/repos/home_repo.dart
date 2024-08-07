a5ed8c22cb91e45b02942f800bc2461923eec39c
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-08-07 20:18:36 +0300

    implement fitech newest books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBoooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBoooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
