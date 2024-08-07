c9c0476033c5ea6c41c8e6e8d93149847b9ab659
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-08-07 21:11:39 +0300

    .

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,7 +4,6 @@ import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -30,11 +29,10 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
     try {
       var data = await apiService.get(
-          endPoint:
-              'volumes?Filtering=free-ebook&q=subject:Programming');
+          endPoint: 'volumes?Filtering=free-ebook&q=subject:Programming');
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -46,4 +44,5 @@ class HomeRepoImpl implements HomeRepo {
       }
       return left(ServerFailure(e.toString()));
     }
+  }
 }
